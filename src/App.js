b423acaa0b5f12e5daceb876c3093f4f73ce9af4
sbstn <hsebastian723@gmail.com>
2022-04-21 13:21:54 -0500

    Se agregan actualizaciones hasta el fin del capitulo 2

--- src/App.js
+++ src/App.js
@@ -2,31 +2,19 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
+import './index.css'
 
 const App = () => {
   const [notes, setNotes] = useState([])  
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-
-  const toggleImportanceOf = id => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = {...note, import: !note.important}
-    // const url = `http://localhost:3001/notes/${id}`
-    // axios.put(url, changedNote).then(response => {
-    //   setNotes(notes.map(note => note.id !== id ? note : response.data))
-    // })
-    noteService
-      .update(id, changedNote)
-      .then(response => {
-        setNotes(notes.map(note => note.id !== id ? note : response.data))
-      })
-  }
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
       .getAll()
-      .then(response => {
-        setNotes(response.data)
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
 
@@ -45,6 +33,29 @@ const App = () => {
   // }, [])
   // console.log('render', notes.length, 'notes');
 
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, import: !note.important}
+    // const url = `http://localhost:3001/notes/${id}`
+    // axios.put(url, changedNote).then(response => {
+    //   setNotes(notes.map(note => note.id !== id ? note : response.data))
+    // })
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -54,8 +65,8 @@ const App = () => {
     }
     noteService
       .create(noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
     // axios
@@ -71,6 +82,32 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+
+    return(
+      <div className='error'>
+        {message}
+      </div>
+    )
+  }
+
+  const Footer = () => {
+    const footerStyle = {
+      color: 'green',
+      fontStyle: 'italic',
+      fontSize: 16
+    }
+    return (
+      <div style={footerStyle}>
+        <br />
+        <em>Note app, Demo prueba </em>
+      </div>
+    )
+  }
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
@@ -79,6 +116,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -99,6 +137,7 @@ const App = () => {
           onChange={handleNoteChange}/>
         <button type='submit'>save</button>
       </form>
+      <Footer />
     </div>
   )
 }
